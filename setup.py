394d99a0c07466ad53088870ee727e416539637c
Petri Savolainen <petri.savolainen@koodaamo.fi>
2014-12-04 16:55:54 +0200

    bump version

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ test_requirements = [
 
 setup(
     name='iconframer',
-    version='0.1.1',
+    version='0.2',
     description='Generate SVG icons',
     long_description=readme + '\n\n' + history,
     author='Petri Savolainen',
